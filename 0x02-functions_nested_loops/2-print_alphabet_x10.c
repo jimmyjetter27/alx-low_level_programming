dc4d900f19fd625bae13a24ec7eba46818e6d7dd
James Atiah <jimmyjetter27@gmail.com>
2022-10-18 14:45:18 -0700

    pushing assignment 2 again

--- 0x02-functions_nested_loops/2-print_alphabet_x10.c
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -6,15 +6,13 @@
 
 void print_alphabet_x10(void)
 {
-	int i;
+	int i = 0;
 	char c;
 
-	for (i = 0; i < 10; ++i)
+	while (i++ <= 9)
 	{
 		for (c = 'a'; c <= 'z'; c++)
-		{
 			_putchar(c);
-		}
-		_putchar('\n');
+		_putchar('/n');
 	}
 }
