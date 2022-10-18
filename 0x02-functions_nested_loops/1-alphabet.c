39cb8fc12dd164b528865c86eb1df2231217c57c
James Atiah <jimmyjetter27@gmail.com>
2022-10-18 14:26:23 -0700

    assignment 1

--- 0x02-functions_nested_loops/1-alphabet.c
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -1,13 +1,16 @@
 #include "main.h"
 
 /**
- * main - print lowercase"
- *
- * Return: 0
+ * print_alphabet - print all alphabets in lowercase
  */
 
-int main(void)
+void print_alphabet(void)
 {
-	print_alphabet();
-	return (0);
+	char c;
+
+	for (c = 'a'; c <= 'z'; c++)
+	{
+		_putchar(c);
+	}
+	_putchar('\n');
 }
