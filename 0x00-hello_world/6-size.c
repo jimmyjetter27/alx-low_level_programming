01ab66b097a0403227b567c9de4eec9421380849
James Atiah <jimmyjetter27@gmail.com>
2022-10-18 08:20:44 -0700

    pushing README.md file

---
+++ 0x00-hello_world/6-size.c
@@ -0,0 +1,11 @@
+#include <stdio.h>
+
+int main(void)
+{
+	printf("Size of a char: %d byte(s)\n", sizeof(char));
+	printf("Size of int: %d byte(s)\n", sizeof(int));
+	print("Size of a long int: %d byte(s)\n", sizeof(long int));
+	print("Size of a long long int: %d byte(s)\n", sizeof(long long int));
+	printf("Size of a float: %d byte(s)\n", sizeof(float));
+	return (0);
+}
