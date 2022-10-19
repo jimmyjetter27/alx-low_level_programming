f294f50850014c0e0f6abf275f499825851e3e56
James Atiah <jimmyjetter27@gmail.com>
2022-10-19 15:24:09 -0700

    assignment 10

---
+++ 0x02-functions_nested_loops/10-add.c
@@ -0,0 +1,11 @@
+#include "main.c"
+/**
+ * add - adds two integers and returns the result.
+ * @i: The first number to be added.
+ * @j: The second number to be added.
+ * Return: sum of two numbers.
+ */
+int add(int i, int j)
+{
+	return (i + j);
+}
