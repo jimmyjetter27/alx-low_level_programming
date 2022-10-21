a22443a6941413432d0fe88266dd0a9283cf7421
James Atiah <jimmyjetter27@gmail.com>
2022-10-20 22:21:57 -0700

    assignment 5

--- 0x04-more_functions_nested_loops/4-print_most_numbers.c
+++ 0x04-more_functions_nested_loops/4-print_most_numbers.c
@@ -1,7 +1,8 @@
 #include "main.h"
 
+
 /**
- * print_most_numbers - this function prints the numbers from 0-9
+ * print_most_numbers - this function prints the numbers from 0 to 9
  *
  * Return: 0-9, without adding 2 and 4, followed by new line
  */
